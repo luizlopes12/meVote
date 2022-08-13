5b4dc09e468ab3bd68331113a0e82cdefebf73cc
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-08-13 15:06:39 -0300

    update

--- src/pages/CandidatesPage/index.jsx
+++ src/pages/CandidatesPage/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Styles from './styled'
 const CandidatesPage = () => {
   return (
-    <div>CandidatesPage</div>
+    <Styles>CandidatesPage</Styles>
   )
 }
 
---
+++ src/pages/CandidatesPage/styled.js
@@ -0,0 +1,10 @@
+import styled, {keyframes} from 'styled-components'
+
+const Styles = styled.section`
+    min-width: 100%;
+    min-height: 100vh;
+    background: rgb(255,247,136);
+    background: linear-gradient(150deg, rgba(255,247,136,1) 0%, rgba(244,228,9,1) 50%, rgba(255,209,0,1) 100%);
+`;
+
+export default Styles
