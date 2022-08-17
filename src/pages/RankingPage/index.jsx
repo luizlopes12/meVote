5933d964b6e909c274e2f1de4ce084cc9c164bda
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-08-17 17:26:16 -0300

    epa

--- src/pages/RankingPage/index.jsx
+++ src/pages/RankingPage/index.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React,{useState, useEffect} from 'react'
+import Styles from './styled'
+import API from '../../config/API'
 const RankingPage = () => {
+  const [votesData, setVotesData] = useState([])
+  useEffect(()=>{
+    API.get('/candidates/ranking')
+    .then((response)=>{
+      setVotesData(response.data)
+    })
+  },[])
   return (
-    <div>RankingPage</div>
+    <Styles>
+      <header className='header'>
+          <div className="__title">
+              Ranking de Votos
+          </div>
+          <div className="__subtitle">
+              {votesData.length} Participantes
+          </div>
+      </header>
+      <section className="listing">
+        {votesData.map((item)=>item.votes)}
+      </section>
+    </Styles>
   )
 }
 
---
+++ src/pages/RankingPage/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+const Styles = styled.section`
+
+`;
+export default Styles
