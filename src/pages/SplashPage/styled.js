7a8a2a074b4e0f215897489c90dc15cbe64ac250
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-08-18 19:42:25 -0300

    Update styled.js

--- src/pages/SplashPage/styled.js
+++ src/pages/SplashPage/styled.js
@@ -123,7 +123,7 @@ const Styles = styled.section`
             margin-top: 80px;
             .__logo{
                 img{
-                    width: 150px;
+                    width: 200px;
                 }
             }
             .__title{
@@ -136,74 +136,10 @@ const Styles = styled.section`
             }
             .__subtitle{
                 color: #fff;
-                font-size: 1.5rem;
+                font-size: 1.8rem;
             }
         }
     }   
 `;
 
 export default Styles
-
-/*
-.loading{
-    width:200px;
-    height:60px;
-    position: absolute;
-    left:50%;
-    top:70%;
-    transform: translate(-50%, -50%);
-}
-.loading span{
-    position: absolute;
-    top:75px;
-    font-size: 20px;
-    letter-spacing: 6px;
-    color: #DBDBDB;
-    left:10%;
-}
-.circle{
-    width:20px;
-    height:20px;
-    position: absolute;
-    border-radius: 50%;
-    background-color: #dbdbdb;
-    left:15%;
-    transform-origin: 50%;
-    animation: ${circle} .5s alternate infinite ease;
-}
-
-
-.circle:nth-child(2){
-    left:45%;
-    animation-delay: .2s;
-}
-.circle:nth-child(3){
-    left:auto;
-    right:15%;
-    animation-delay: .3s;
-}
-.shadow{
-    width:20px;
-    height:4px;
-    border-radius: 50%;
-    background-color: rgba(0,0,0,.5);
-    position: absolute;
-    top:62px;
-    transform-origin: 50%;
-    z-index: -1;
-    left:15%;
-    filter: blur(1px);
-    animation: ${shadow} .5s alternate infinite ease;
-}
-
-
-.shadow:nth-child(4){
-    left: 45%;
-    animation-delay: .2s
-}
-.shadow:nth-child(5){
-    left:auto;
-    right:15%;
-    animation-delay: .3s;
-}
-*/
